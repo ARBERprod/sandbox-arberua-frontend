c0006466a5ab8c89c289a3af561165f68dbc64d0
Novikov <ruslan.n@hypecharge.io>
2026-02-16 20:38:18 +0200

    Revert "Revert "Remove dead Sentry integration""
    This reverts commit 68eec5a96327d4c9e49f12cd0474908ad996b3ec.

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,12 +1,7 @@
-import * as Sentry from '@sentry/nextjs';
 import NextErrorComponent from 'next/error';
 
 const Error = ({ statusCode }: any) => <NextErrorComponent statusCode={statusCode} />;
 
-Error.getInitialProps = async (contextData: any) => {
-  await Sentry.captureUnderscoreErrorException(contextData);
-
-  return NextErrorComponent.getInitialProps(contextData);
-};
+Error.getInitialProps = async (contextData: any) => NextErrorComponent.getInitialProps(contextData);
 
 export default Error;
