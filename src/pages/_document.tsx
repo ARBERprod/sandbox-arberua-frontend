1f3515edc8c45fcec685a351d8d26cf4f77ddbe3
Novikov <ruslan.n@hypecharge.io>
2026-02-16 20:38:17 +0200

    Revert "Revert "Remove dead GTM and Facebook Pixel noscript fallbacks from _document""
    This reverts commit 763f37036b6cfaea3b641a448f647fa47edc8ba9.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,26 +11,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        {/* Google Tag Manager (noscript) */}
-        {/* <noscript> */}
-        {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
-        {/* <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PHHD7CTQ"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript> */}
-        {/* End Google Tag Manager (noscript) */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            alt=""
-            src="https://www.facebook.com/tr?id=1440826369354364&ev=PageView&noscript=1"
-          />
-        </noscript>
       </body>
     </Html>
   );
