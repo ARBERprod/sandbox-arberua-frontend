45c463da8e336f5eef24ab607706780847cf9b17
Novikov <ruslan.n@hypecharge.io>
2026-02-16 18:39:21 +0200

    SelectField common component - fix search bug

--- src/shared/lib/dl.ts
+++
@@ -1,5 +0,0 @@
-export function dlPush(payload: Record<string, any>) {
-  if (typeof window === 'undefined') return;
-  (window as any).dataLayer = (window as any).dataLayer || [];
-  (window as any).dataLayer.push(payload);
-}
